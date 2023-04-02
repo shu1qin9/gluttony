0699a1ac8bc093350b326f2fef27a06159579c48
温酒叙余生 <5568440+shuiqing_liu@user.noreply.gitee.com>
2023-04-02 14:22:04 +0800

    彻底修复扫描漏洞是会扫描lib目录的BUG

--- lib/core/init/init.py
+++ lib/core/init/init.py
@@ -3,6 +3,7 @@
 初始化扫描器
 """
 import os
+import fnmatch
 import platform
 import importlib
 from lib.core.base import variable
@@ -12,27 +13,18 @@ from lib.core.error.exception import ExpliotException
 
 
 # 遍历枚举路劲下的所有文件
-def get_files_path(file_path):
-    # 数据初始化
+def get_files_path(file_path, black_list):
     file_list = []
-    config = Config()
     if os.path.isdir(file_path):
         for check_file in os.listdir(file_path):
             current_path = os.path.join(file_path, check_file)
-            exploits_black_list = config.read_ini("GLUTTONY", "BLACK_LIST", isjson=True).get("exploits")
-            # 判断当前绝对路劲是否为python文件
-            if (os.path.isfile(current_path) and check_file.split('.')[-1] != 'py') or any(
-                    current_path.startswith(path) for path in exploits_black_list):
+            if os.path.isfile(current_path) and check_file.split('.')[-1] != 'py':
                 continue
-            each_path = get_files_path(current_path)
-            # 加载黑名单
+            if any(fnmatch.fnmatch(current_path, pattern) for pattern in black_list):
+                continue
+            each_path = get_files_path(current_path, black_list)
             for file in each_path:
-                # 判断文件是否在黑名单，如果存在则不予记录
-                if get_module_name(file) in exploits_black_list:
-                    continue
-                else:
-                    file_list.append(file)
-
+                file_list.append(file)
     else:
         file_list.append(file_path)
     return file_list
@@ -64,8 +56,9 @@ def get_exploits_list(file_path):
     # 基本信息初始化
     exp_module_list = []
     root_path = variable.get_arg("root_path")
+    black_list = Config().read_ini("GLUTTONY", "BLACK_LIST", isjson=True).get("exploits")
     exp_file_path = os.path.join(root_path, file_path)
-    exp_path_list = get_files_path(exp_file_path)
+    exp_path_list = get_files_path(exp_file_path, black_list)
     for exp_path in exp_path_list:
         exp_path = exp_path.replace(root_path, "")
         exp_module_path = get_module_path(exp_path)
@@ -80,8 +73,9 @@ def search_exploits(file_path, module_list):
     console = Console()
     exp_module_list = []
     root_path = variable.get_arg("root_path")
+    black_list = Config().read_ini("GLUTTONY", "BLACK_LIST", isjson=True).get("exploits")
     exp_file_path = os.path.join(root_path, file_path)
-    exp_path_list = get_files_path(exp_file_path)
+    exp_path_list = get_files_path(exp_file_path, black_list)
     # 循环遍历查询
     for module in module_list:
         for exp_path in exp_path_list:
